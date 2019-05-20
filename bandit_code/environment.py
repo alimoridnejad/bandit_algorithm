108789a55e8589c4e45ba605dc5597a5593b7e20
niloufar <faghihi.niloufar@gmail.com>
2019-05-20 19:10:15 -0400

    It's better to use numpy built-in Beroulli distribution. Add doc string to the function so that the user can better understand the method.

--- bandit_code/environment.py
+++ bandit_code/environment.py
@@ -6,12 +6,16 @@ class BernoulliEnv(object):
 
     def __init__(self, N_arm):
         self.N_arm = N_arm
-        # Draw means from probability distribution
+        # Draw true parameters
+        # Here parameter is true probability of success for each arm and also the expected reward in each sampling
+        # from the Bernoulli distribution.
         self.True_probs = np.random.normal(0, 1, N_arm)
 
-    def generate_reward(self, i):
-        # The player selected the i-th machine.
-        if np.random.random() < self.True_probs[i]:
-            return 1
-        else:
-            return 0
+    def generate_reward(self, arm):
+        """
+        This method gives a binary value of 0 or 1 for the selected arm.
+        :param arm: selected arm
+        :return: sample reward
+        """
+        sample = np.random.binomial(n=1, p=self.True_probs[arm])
+        return sample
