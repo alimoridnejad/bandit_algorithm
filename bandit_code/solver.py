ee25fe6d8dd2c8865cd4b1bad7e8ffeadb84cdc5
niloufar <faghihi.niloufar@gmail.com>
2019-05-20 20:37:33 -0400

    to get the best reward, we need to add cumulative reward

--- bandit_code/solver.py
+++ bandit_code/solver.py
@@ -1,15 +1,12 @@
 from __future__ import division
 
-from typing import Type
-
-from bandit_code.environment import bernoulli_env
 import numpy as np
 
-from bandit_code.environment import BernoulliEnv
+from bandit_code.environment import bernoulli_env
 
 
 class Solver(object):
-    def __init__(self, bernoulli_env: Type[BernoulliEnv] = bernoulli_env):
+    def __init__(self, bernoulli_env: bernoulli_env):
         """The target Environment (env) to solve."""
 
         # The environment.py initialization
@@ -21,19 +18,21 @@ class Solver(object):
     def run(self, num_steps):
         for _ in range(num_steps):
             self.run_one_step()
+        return self.total_reward
 
 
 class EpsilonGreedy(Solver):
-    def __init__(self, eps):
+    def __init__(self, bernoulli_env, eps):
         # Running env from the parent class
-        super(EpsilonGreedy, self).__init__(bernoulli_env)
+        super().__init__(bernoulli_env)
         # The probability to explore at each time step
         self.eps = eps
         # Initial estimates
         self.estimates = np.random.normal(0, 1, self.bernoulli_env.N_arm)
+        self.total_reward = 0
 
     def arm_selection(self):
-        sample = np.random.binomial(n=1, p=self.eps)
+        sample = np.random.binomial(n=1, p=1-self.eps)
         if sample == 0:
             # Do a random exploration
             i = np.random.choice(self.bernoulli_env.N_arm)
@@ -51,7 +50,12 @@ class EpsilonGreedy(Solver):
     def estimate_update(self, arm, reward):
         self.estimates[arm] += 1. / (self.counts[arm] + 1) * (reward - self.estimates[arm])
 
+    def cumulative_reward(self,reward):
+        self.total_reward += reward
+
     def run_one_step(self):
         arm = self.arm_selection()
         reward = self.reward_observation(arm)
+        self.cumulative_reward(reward)
         self.estimate_update(arm, reward)
+
