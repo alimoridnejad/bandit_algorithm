7b7eaec546d826b09c87cac931e515dae81fbe28
niloufar <faghihi.niloufar@gmail.com>
2019-05-20 20:38:53 -0400

    main block takes in all necessary parameters. It run an experiment using the two input: number of arms and number of iterations

---
+++ bandit_code/main.py
@@ -0,0 +1,17 @@
+from bandit_code.solver import EpsilonGreedy
+from bandit_code.environment import BernoulliEnv
+
+
+def Experiment(N_arm, Time_horizon):
+    """Run a small experiment on solving a Bernoulli bandit problem."""
+
+    bernoulli_env = BernoulliEnv(N_arm)
+    learner_eps = EpsilonGreedy(bernoulli_env, 0.1)
+    total_reward = learner_eps.run(Time_horizon)
+    print(f"Learner average reward is {total_reward/Time_horizon}")
+    best_reward = bernoulli_env.get_best_reward()
+    print(f"Optimal average reward is {best_reward*Time_horizon/Time_horizon}")
+
+
+if __name__ == "__main__":
+    Experiment(5, 100000)
