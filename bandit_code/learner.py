8a8a593e8ee45c243dbba26c6e27ffdd8f18d348
McHeartsbane <nima.akbarzadeh@mail.mcgill.ca>
2019-05-25 19:52:18 -0400

    Some of the functions and initializations have been moved to the parent class as they are being used in the child classes. Three learning algorithms have been added as three child classes.

--- bandit_code/learner.py
+++ bandit_code/learner.py
@@ -2,7 +2,7 @@ from __future__ import division
 
 import numpy as np
 
-from bandit_code.environment import bernoulli_env
+from environment import bernoulli_env
 
 
 class Learner(object):
@@ -11,9 +11,27 @@ class Learner(object):
 
         # The environment.py initialization
         self.bernoulli_env = bernoulli_env
-
         # To count the number of times each arm is selected, let's create an empty vector at the size of number of arms
         self.counts = np.zeros(shape=self.bernoulli_env.N_arm)
+        # Initial estimates
+        self.estimates = np.random.uniform(0, 1, self.bernoulli_env.N_arm)
+        self.total_reward = 0
+        # Number of times the algorithm has been being applied
+        self.time = 0
+
+    def reward_observation(self, arm):
+        # Generate a reward from environment.py
+        reward = self.bernoulli_env.generate_reward(arm)
+        return reward
+
+    def cumulative_reward(self,reward):
+        self.total_reward += reward
+
+    def run_one_step(self):
+        arm = self.arm_selection()
+        reward = self.reward_observation(arm)
+        self.cumulative_reward(reward)
+        self.estimate_update(arm, reward)
 
     def run(self, num_steps):
         for _ in range(num_steps):
@@ -27,13 +45,10 @@ class EpsilonGreedy(Learner):
         super().__init__(bernoulli_env)
         # The probability to explore at each time step
         self.eps = eps
-        # Initial estimates
-        self.estimates = np.random.uniform(0, 1, self.bernoulli_env.N_arm)
-        self.total_reward = 0
 
     def arm_selection(self):
-        sample = np.random.binomial(n=1, p=1-self.eps)
-        if sample == 0:
+        sample = np.random.binomial(n=1, p=self.eps)
+        if sample == 1:
             # Do a random exploration
             i = np.random.choice(self.bernoulli_env.N_arm)
         else:
@@ -42,20 +57,73 @@ class EpsilonGreedy(Learner):
         self.counts[i] += 1
         return i
 
-    def reward_observation(self, arm):
-        # Generate a reward from environment.py
-        reward = self.bernoulli_env.generate_reward(arm)
-        return reward
+    def estimate_update(self, arm, reward):
+        self.estimates[arm] += 1. / (self.counts[arm] + 1) * (reward - self.estimates[arm])
+
+class DecayingEpsilonGreedy(Learner):
+    def __init__(self, bernoulli_env, eps, beta):
+        # Running env from the parent class
+        super().__init__(bernoulli_env)
+        # The probability to explore at each time step
+        self.eps = eps
+        # The decaying factor
+        self.beta = beta
+
+    def arm_selection(self):
+        sample = np.random.binomial(n=1, p=self.eps)
+        if sample == 1:
+            # Do a random exploration
+            i = np.random.choice(self.bernoulli_env.N_arm)
+        else:
+            # Pick the best estimate
+            i = np.argmax(self.estimates)
+        self.counts[i] += 1
+        # Decay the value of the epsilon using the formula eps = eps/(1+time*beta)
+        self.eps = self.eps/(1+self.time*self.beta)
+        return i
 
     def estimate_update(self, arm, reward):
         self.estimates[arm] += 1. / (self.counts[arm] + 1) * (reward - self.estimates[arm])
 
-    def cumulative_reward(self,reward):
-        self.total_reward += reward
 
-    def run_one_step(self):
-        arm = self.arm_selection()
-        reward = self.reward_observation(arm)
-        self.cumulative_reward(reward)
-        self.estimate_update(arm, reward)
+class UpperConfidenceBound(Learner):
+    def __init__(self, bernoulli_env):
+        # Running env from the parent class
+        super().__init__(bernoulli_env)
 
+    def arm_selection(self):
+        self.time += 1
+        # Pick the best one with consideration of upper confidence bounds.
+        i = max(range(self.bernoulli_env.N_arm), key=lambda x: self.estimates[x] + np.sqrt(
+            2 * np.log(self.time) / (1 + self.counts[x])))
+        self.counts[i] += 1
+        return i
+
+    def estimate_update(self, arm, reward):
+        self.estimates[arm] += 1. / (self.counts[arm] + 1) * (reward - self.estimates[arm])
+
+
+class ThompsonSampling(Learner):
+    def __init__(self, bernoulli_env, init_success=1, init_failure=1):
+        """
+        init_success (int): initial value of a in Beta(a, b).
+        init_failure (int): initial value of b in Beta(a, b).
+        """
+        super(ThompsonSampling, self).__init__(bernoulli_env)
+        # Number of successes and failures for each arm
+        self.N_success = init_success*np.ones(shape=self.bernoulli_env.N_arm)
+        self.N_failure = init_failure*np.ones(shape=self.bernoulli_env.N_arm)
+
+    def arm_selection(self):
+        self.time += 1
+        # Sample each arm according to the current posterior distribution over each arm
+        samples = [np.random.beta(self.N_success[x], self.N_failure[x]) for x in range(self.bernoulli_env.N_arm)]
+        # Pick the arm with the best sampled value
+        i = max(range(self.bernoulli_env.N_arm), key=lambda x: samples[x])
+        self.counts[i] += 1
+        return i
+
+    def estimate_update(self, arm, reward):
+        self.N_success[arm] += reward
+        self.N_failure[arm] += (1 - reward)
+        self.estimates[arm] = self.N_success[arm] / (self.N_success[arm] + self.N_failure[arm])
