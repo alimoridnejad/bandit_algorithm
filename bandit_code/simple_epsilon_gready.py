9caae2fa23b547a225662957fc049beed42739cd
niloufar <faghihi.niloufar@gmail.com>
2019-06-02 14:32:42 -0400

    write epsilon greedy class

---
+++ bandit_code/simple_epsilon_gready.py
@@ -0,0 +1,38 @@
+import random
+
+import numpy as np
+
+
+class EpsilonGreedy(object):
+    def __init__(self, epsilon: float = 0.1, counts: int = None, values: float = None):
+        self.epsilon = epsilon
+        self.counts = counts
+        self.values = values
+
+    def pre_populate_counts_and_values(self, n_arms:int):
+        self.counts = np.zeros(n_arms, dtype=int)
+        self.values = np.zeros(n_arms)
+
+    def pick_arm(self) -> int:
+        sample = np.random.binomial(n=1 , p=self.epsilon)
+
+        if sample == 1:
+            # explore
+            picked_arm = random.choice(list(enumerate(self.values)))[0]
+        else:
+            # exploit
+            picked_arm = np.argmax(self.values)
+
+        return picked_arm
+
+    def update(self, values, picked_arm, reward):
+
+        # update count for picked arm
+        self.counts[picked_arm] += 1
+
+        # update the value
+        arm_count = self.counts[picked_arm]
+        arm_value = self.values[picked_arm]
+        self.values[picked_arm] = arm_value * ((arm_count- 1)/arm_count) + reward/arm_count
+
+        
